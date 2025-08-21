bb3e0fc5573e849c5424019bba2f1775ed9ecc9f
yunseong <gkrlsanjf@naver.com>
2025-08-21 17:48:40 +0900

    Color 디자인 시스템 추가

--- shared/src/commonMain/kotlin/world/pople/oper/core/designsystem/theme/Color.kt
+++ shared/src/commonMain/kotlin/world/pople/oper/core/designsystem/theme/Color.kt
@@ -1,32 +1,145 @@
 package world.pople.oper.core.designsystem.theme
+
 import androidx.compose.ui.graphics.Color
 
-// Light Color
-val Primary100 = Color(0xFFEAE2FF)
-val Primary500 = Color(0xFF7C5FF3)
-
 val White = Color(0xFFFFFFFF)
-val Black100 = Color(0xFF1B1B1B)
-val Default = Color(0xFF1B1B1B)
+val PaleGray = Color(0xFFF9F9F9)
+val PaperGray = Color(0xFFFDFDFD)
+val LightGray = Color(0xFFDCDCDC)
+val Gray = Color(0xFFC2C2C2)
+val DuskGray = Color(0xFF868686)
+val DarkGray = Color(0xFF5E5E5E)
+val Black = Color(0xFF000000)
+val Graphite = Color(0xFF292929)
 
-val Gray50 = Color(0xFFF7F7F7)
+val Purple01 = Color(0xFFB469FF)
+val Purple01A30 = Color(0x4DB469FF)
+val Mint01 = Color(0xFF7BFAF2)
+
+val Neon01 = Color(0xFF49F300)
+val Neon01A50 = Color(0x8049F300)
+val Neon02 = Color(0xFF6CFF2D)
+val Neon03 = Color(0xFF94FB67)
+val Neon04 = Color(0xFFA1FF78)
+val Neon05 = Color(0xFFEEFFE7)
+val Neon05A40 = Color(0x66EEFFE7)
+
+val Green01 = Color(0xFF88F21D)
+val Green02 = Color(0xFF6BEB33)
+val Green03 = Color(0xFF52C520)
+val Green03A30 = Color(0x4D52C520)
+val Green04 = Color(0xFF386524)
+
+val Yellow01 = Color(0xFFF2E800)
+val Yellow02 = Color(0xFFE8ED00)
+val Yellow03 = Color(0xFFBEED00)
+val Yellow03A40 = Color(0x66A1ED00)
+val Yellow04 = Color(0xFF465703)
+val Yellow05 = Color(0xFF222B00)
+
+val Red01 = Color(0xFFF9DEDC)
+val Red02 = Color(0xFFF2B8B5)
+val Red03 = Color(0xFFB3261E)
+val Red04 = Color(0xFF8C1D18)
+val Red05 = Color(0xFF601410)
+val Red06 = Color(0xFF410E0B)
+
+val Cosmos = Color(0xFF151515)
+
+val Black100 = Color(0xFF000000)
+
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val mainColor = Color(0xFFB693FB)
+val disableColor = Color(0xFFEBEBEB)
+val disableButton = Color(0xFFBFBFBF)
+val ableButton = Color(0xFF5D38E5)
+
+// POPLE Color
+// https://www.figma.com/design/uZC4kIA3bLL2cmrseTxLkC/%5BAPP%5D-WF-%2F-DS-%2F-GUI-_V2?node-id=55-2188&t=7IEe4bMi2IU7KFkN-1
+val Primary50 = Color(0xFFF7F6FD)
+val Primary100 = Color(0xFFEFEBFC)
+val Primary200 = Color(0xFFC7C6FB)
+val Primary300 = Color(0xFFB693FB)
+val Primary400 = Color(0xFF8F78E2)
+val Primary500 = Color(0xFF5D38E5)
+val Primary600 = Color(0xFF3918B4)
+
+val Secondary100 = Color(0xFFF6FEFD)
+val Secondary200 = Color(0xFFD1FAF5)
+val Secondary300 = Color(0xFFA2F6EA)
+val Secondary400 = Color(0xFF60EFDB)
+val Secondary500 = Color(0xFF16DEC2)
+val Secondary600 = Color(0xFF14C3AA)
+
+val Gray50 = Color(0xFFF9F9F9)
 val Gray100 = Color(0xFFF2F2F2)
 val Gray200 = Color(0xFFEBEBEB)
-val Gray300 = Color(0xFFDCDCDC)
-val Gray400 = Color(0xFFC4C4C4)
-val Gray500 = Color(0xFFAFAFAF)
-val Gray600 = Color(0xFF8E8E8E)
-val Gray800 = Color(0xFF424242)
-val Gray900 = Color(0xFF2C2C2C)
+val Gray300 = Color(0xFFD9D9D9)
+val Gray400 = Color(0xFFBFBFBF)
+val Gray500 = Color(0xFFB3B3B3)
+val Gray600 = Color(0xFF999999)
+val Gray700 = Color(0xFF676665)
+val Gray800 = Color(0xFF4D4C4C)
+val Gray900 = Color(0xFF343332)
 
-val Error = Color(0xFFD94C4C)
-val Progress = Color(0xFF405ED9)
-val Success = Color(0xFF1CBA76)
-val Warning = Color(0xFFF3BE37)
-val Warning2 = Color(0xFFEA8832)
+val BaseWhite = Color(0xFFF7F6FD)
+val BaseDimmer = Color(0xFF000000).copy(alpha = 0.4f)
+val Default = Color(0xFF1B1918)
 
-val BackgroundError = Color(0xFFF3C6C6)
-val BackgroundProgress = Color(0xFFC5CEF4)
-val BackgroundSuccess = Color(0xFFD2F4E4)
-val BackgroundWarning = Color(0xFFF1E3C1)
-val BackgroundWarning2 = Color(0xFFF7E1CD)
+val Error = Color(0xFFFF4C4C)
+val Warning = Color(0xFFFFCD4C)
+val Warning2 = Color(0xFFFFA04C)
+val Success = Color(0xFF2BCA86)
+val Progress = Color(0xFF4C6FFF)
+
+val BackgroundError = Color(0xFFFFE0E0)
+val BackgroundWarning = Color(0xFFFFF6E0)
+val BackgroundWarning2 = Color(0xFFFFEFE0)
+val BackgroundSuccess = Color(0xFFEBFAF3)
+val BackgroundProgress = Color(0xFFE0E6FF)
+
+val ToolingRed100 = Color(0xFFFFF4F4)
+val ToolingRed200 = Color(0xFFFEC7C5)
+val ToolingRed300 = Color(0xFFFCA4A1)
+val ToolingRed400 = Color(0xFFFB716D)
+val ToolingRed500 = Color(0xFFF9423C)
+
+val ToolingPink100 = Color(0xFFFEF3F8)
+val ToolingPink200 = Color(0xFFFFE0EE)
+val ToolingPink300 = Color(0xFFFFCCE5)
+val ToolingPink400 = Color(0xFFFFBADA)
+val ToolingPink500 = Color(0xFFFFA3CE)
+
+val ToolingGreen100 = Color(0xFFF9FCE8)
+val ToolingGreen200 = Color(0xFFEDF6BA)
+val ToolingGreen300 = Color(0xFFDDEE7F)
+val ToolingGreen400 = Color(0xFFD0E750)
+val ToolingGreen500 = Color(0xFFC0DF16)
+
+val ToolingPurple100 = Color(0xFFF7F6FD)
+val ToolingPurple200 = Color(0xFFC7C6FB)
+val ToolingPurple300 = Color(0xFFB693FB)
+val ToolingPurple400 = Color(0xFF8F78E2)
+val ToolingPurple500 = Color(0xFF5D38E5)
+
+val ToolingMint100 = Color(0xFFF6FEFD)
+val ToolingMint200 = Color(0xFFD1FAF5)
+val ToolingMint300 = Color(0xFFA2F6EA)
+val ToolingMint400 = Color(0xFF60EFDB)
+val ToolingMint500 = Color(0xFF16DEC2)
+
+val ToolingBlue100 = Color(0xFFF5FCFE)
+val ToolingBlue200 = Color(0xFFCDF1F8)
+val ToolingBlue300 = Color(0xFF9BE2F2)
+val ToolingBlue400 = Color(0xFF74D7ED)
+val ToolingBlue500 = Color(0xFF38C6E5)
+
+
